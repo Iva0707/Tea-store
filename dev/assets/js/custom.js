1e18db3a4a8754ef1c153eb107986d8163042409
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-09-29 20:16:44 +0200

    header

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,5 +1,12 @@
-const burger = document.getElementById('burger')
+const burger = document.getElementById('burger');
+const burgerText = document.getElementById('burder_text');
+const headerNav = document.getElementById('header_nav');
+const body = document.querySelector('body');
+
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('header__burger-open')
+    burger.classList.toggle('header__burger-open');
+    burgerText.classList.toggle('header__burger-open');
+    headerNav.classList.toggle('header__burger-open');
+    body.classList.toggle('header__burger-open')
 })
